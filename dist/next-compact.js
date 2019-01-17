4325f1dca253fda536e00e3d503ae81246d17fda
afeiship <afeiship@gmail.com>
2019-01-17 20:49:22 +0800

    feat: add object compact

--- dist/next-compact.js
+++ dist/next-compact.js
@@ -2,12 +2,14 @@
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
 
-  nx.compact = function(inArray) {
-    var result = [];
-    for (var index = 0; index < inArray.length; index++) {
-      var value = inArray[index];
-      value && result.push(value);
-    }
+  nx.compact = function(inTarget) {
+    var isary = inTarget instanceof Array;
+    var result = isary ? [] : {};
+    nx.each(inTarget, function(key, value) {
+      if (value) {
+        isary ? result.push(value) : (result[key] = value);
+      }
+    });
     return result;
   };
 
--- test/test.js
+++ test/test.js
@@ -3,9 +3,21 @@ require('../src/next-compact');
 
 describe('nx.remove', function() {
   var arr1 = [0, 1, false, 2, '', 3];
+  var obj = {
+    name: 'fei',
+    age: 0,
+    test: 12,
+    ni: null,
+    tes2: undefined
+  };
 
-  test('return truthy vaulle', function() {
+  test('array: return truthy vaulle', function() {
     var result = nx.compact(arr1);
     expect(result).toEqual([1, 2, 3]);
+  });
+
+  test('object: return truthy vaulle', function() {
+    var result = nx.compact(obj);
+    expect(result).toEqual({ name: 'fei', test: 12 });
   });
 });
