b2f99abbf0f6dfa1ec48f4e61200f11b4ac397ec
afeiship <afeiship@gmail.com>
2019-01-17 17:10:44 +0800

    feat: refactor

--- dist/next-compact.js
+++ dist/next-compact.js
@@ -1,20 +1,17 @@
-(function () {
-
+(function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
 
-  nx.compact = function(inArray){
+  nx.compact = function(inArray) {
     var result = [];
     for (var index = 0; index < inArray.length; index++) {
       var value = inArray[index];
-      value && result.push( value );
+      value && result.push(value);
     }
     return result;
   };
 
-
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = nx.compact;
   }
-
-}());
+})();
